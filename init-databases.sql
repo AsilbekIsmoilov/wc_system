51baedfb7b13a1028237d69524d4baf46748b56f
Asilbek Ismoilov <asilbekismoilov212002@gmail.com>
2026-06-15 17:33:34 +0500

    db changed

--- init-databases.sql
+++ init-databases.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE IF NOT EXISTS project1 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE DATABASE IF NOT EXISTS project1_archive CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS wc_system_default CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS wc_system_archive CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
