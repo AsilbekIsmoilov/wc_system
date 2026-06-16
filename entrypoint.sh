e7fe73a473150d99d121cc53e1ed33eee7597103
Asilbek Ismoilov <asilbekismoilov212002@gmail.com>
2026-06-16 12:55:50 +0000

    Edit entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
-
 set -e
-
-if [ "${RUN_MIGRATIONS}" = "1" ]; then
-  python manage.py migrate --noinput
-  python manage.py migrate --database=archive --noinput
-  python manage.py collectstatic --noinput
-  python manage.py shell -c "from django.contrib.auth import get_user_model; U=get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin','','123')"
-
-  ( python daily_runner.py || echo 'daily_runner 1 failed' ) &
-fi
-
 exec "$@"
